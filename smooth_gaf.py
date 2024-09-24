fa429e7eef947624b644897712935a9d930445a8
Luca Denti <ldenti@proton.me>
2024-09-24 16:09:27 +0200

    Format smoother

--- smooth_gaf.py
+++ smooth_gaf.py
@@ -2,17 +2,19 @@ import sys
 import re
 from Bio import SeqIO
 
-rc_map = {"A":"T", "C":"G", "G":"C", "T":"A", "N":"N"}
+rc_map = {"A": "T", "C": "G", "G": "C", "T": "A", "N": "N"}
+
 
 def split(cigar):
     pairs = []
-    p = re.compile(r"([0-9]+[=XID])") # TODO check if these are enough
+    p = re.compile(r"([0-9]+[=XID])")  # TODO check if these are enough
     # TODO check why we have empty strings
     for x in p.split(cigar):
         if x == "":
             continue
         pairs.append((int(x[:-1]), x[-1]))
     return pairs
+
 
 def compact(cigar):
     ccigar = [cigar[0]]
@@ -23,21 +25,33 @@ def compact(cigar):
             ccigar.append((l, op))
     return ccigar
 
+
 def cigar2str(cigar):
     return "cg:Z:" + "".join(f"{l}{op}" for l, op in cigar)
+
 
 def rc(S):
     return "".join([rc_map[c] for c in S[::-1]])
 
+
 def cut(alignment, interval, gfa_s):
     #   0     1   2   3       4     5     6   7   8         9     10    11   12-
-    qname, qlen, qs, qe, strand, path, plen, ps, pe, residues, ablen, mapq, *opt = alignment
+    qname, qlen, qs, qe, strand, path, plen, ps, pe, residues, ablen, mapq, *opt = (
+        alignment
+    )
     qlen = int(qlen)
     cigar = split((opt[-1].split(":")[2]))
 
-    path_seq = "".join([gfa_s[node] if path[0] == ">" else rc(gfa_s[node]) for node in path[1:].split(path[0])])[int(ps):int(pe)+1] # CHECKME pe+1 ?
-    pp = 0 # position on path
-    qp = int(qs) # position on query
+    path_seq = "".join(
+        [
+            gfa_s[node] if path[0] == ">" else rc(gfa_s[node])
+            for node in path[1:].split(path[0])
+        ]
+    )[
+        int(ps) : int(pe) + 1
+    ]  # CHECKME pe+1 ?
+    pp = 0  # position on path
+    qp = int(qs)  # position on query
     ccigar = []
     addflag = False
     stopflag = False
@@ -46,24 +60,28 @@ def cut(alignment, interval, gfa_s):
     pkept = 0
     pskipped2 = 0
     for opl, op in cigar:
-        if not addflag and op != "D" and (qp == interval[0] or (qp < interval[0] and qp + opl > interval[0])):
+        if (
+            not addflag
+            and op != "D"
+            and (qp == interval[0] or (qp < interval[0] and qp + opl > interval[0]))
+        ):
             addflag = True
             cut = interval[0] - qp
-        if addflag and op != "D" and (qp < interval[1] and qp+opl >= interval[1]):
+        if addflag and op != "D" and (qp < interval[1] and qp + opl >= interval[1]):
             stopflag = True
-            cut = qp+opl - interval[1]
+            cut = qp + opl - interval[1]
 
         edit = ""
         if op == "=":
             qp += opl
-            pp+= opl
+            pp += opl
         elif op == "X":
-            edit = path_seq[pp:pp+opl]
+            edit = path_seq[pp : pp + opl]
             qp += opl
-            pp+=opl
+            pp += opl
         elif op == "D":
-            edit = path_seq[pp:pp+opl]
-            pp+=opl
+            edit = path_seq[pp : pp + opl]
+            pp += opl
         elif op == "I":
             qp += opl
         else:
@@ -97,9 +115,9 @@ def cut(alignment, interval, gfa_s):
     new_s, new_e = 0, -1
     for node in path[1:].split(path[0]):
         l = len(gfa_s[node])
-        if p + l-1 < s:
-            pass # shift += l
-        elif p < s and p + l -1 >= s:
+        if p + l - 1 < s:
+            pass  # shift += l
+        elif p < s and p + l - 1 >= s:
             # print(f"Starting {p} {s} {p+l-1}", file=sys.stderr)
             new_path.append(str(node))
             new_path_l += l
@@ -107,17 +125,17 @@ def cut(alignment, interval, gfa_s):
         elif p >= s and p + l - 1 <= e:
             new_path.append(str(node))
             new_path_l += l
-        elif p < e and p+l>= e:
+        elif p < e and p + l >= e:
             # print(f"Ending {p} {p+l} {e}", file=sys.stderr)
             new_path.append(str(node))
             new_path_l += l
-            new_e = new_path_l - (p+l - e)
+            new_e = new_path_l - (p + l - e)
             break
         else:
             # print(f"Ending {p} {p+l} {e}", file=sys.stderr)
             new_e = new_path_l
             break
-        p+=l
+        p += l
     # print("New path lenght:", len(new_path), len(new_path)*32)
     sep = path[0]
     alignment[5] = sep + sep.join(new_path)
@@ -126,12 +144,13 @@ def cut(alignment, interval, gfa_s):
     alignment[8] = new_e
 
 
-
 def get_split(alignments):
     intervals = []
     for al in alignments:
-        qname, qlen, qs, qe, strand, path, plen, ps, pe, residues, ablen, mapq, *opt = al
-        qs, qe= int(qs), int(qe)
+        qname, qlen, qs, qe, strand, path, plen, ps, pe, residues, ablen, mapq, *opt = (
+            al
+        )
+        qs, qe = int(qs), int(qe)
         if len(intervals) == 0 or qs > intervals[-1][1]:
             intervals.append((qs, qe))
         else:
@@ -150,20 +169,22 @@ def smooth(alignments, read, ofq, ogaf, minl=10):
         if int_s > p:
             op = "N"
             if p == 0:
-                op = "H" # TODO S/H if hard clipping
+                op = "H"  # TODO S/H if hard clipping
             # TODO decide if we want to add clips to the sequence
             if op == "N":
                 seq += read[p:int_s]
-            #full_cigar.append((int_s - p, op))
+            # full_cigar.append((int_s - p, op))
             p += int_s - p
 
         local_cigar = []
         plen = 0
-        al[2] += poffset # move starting position
+        al[2] += poffset  # move starting position
         for l, op, c in cigar:
-            assert (op in ["=", "I"] and c == "") or (op in ["X", "D"] and c != ""), f"{l} {op} {c}"
+            assert (op in ["=", "I"] and c == "") or (
+                op in ["X", "D"] and c != ""
+            ), f"{l} {op} {c}"
             if op == "=":
-                seq += read[p:p+l]
+                seq += read[p : p + l]
                 local_cigar.append((l, op))
                 p += l
             elif op == "X":
@@ -172,10 +193,10 @@ def smooth(alignments, read, ofq, ogaf, minl=10):
                 p += l
             elif op == "I":
                 if l < minl:
-                    p+=l
+                    p += l
                     poffset -= l
                 else:
-                    seq += seq[p:p+l]
+                    seq += seq[p : p + l]
                     local_cigar.append((l, op))
                     p += l
             elif op == "D":
@@ -188,7 +209,7 @@ def smooth(alignments, read, ofq, ogaf, minl=10):
             else:
                 assert False, f"Unkown CIGAR operation, {op}, why?"
 
-        al[3] += poffset # move ending position
+        al[3] += poffset  # move ending position
         al[-1] = cigar2str(compact(local_cigar))
         full_cigar += local_cigar
     if p < int(alignments[-1][1]):
@@ -204,9 +225,24 @@ def smooth(alignments, read, ofq, ogaf, minl=10):
 
     for al in alignments:
         # qname, qlen, qs, qe, strand, path, plen, ps, pe, residues, ablen, mapq, *opt
-        print(al[0], len(seq), al[2], al[3], al[4], al[5], al[6], al[7], al[8], "*", "*", al[11], al[-1], sep="\t", file=ogaf) # al[9], al[10],
+        print(
+            al[0],
+            len(seq),
+            al[2],
+            al[3],
+            al[4],
+            al[5],
+            al[6],
+            al[7],
+            al[8],
+            "*",
+            "*",
+            al[11],
+            al[-1],
+            sep="\t",
+            file=ogaf,
+        )  # al[9], al[10],
     alignments.append(len(seq))
-
 
 
 def main():
@@ -242,7 +278,12 @@ def main():
             # we got a new read, so we analyze the current read
             if len(alignments) > 3:
                 # TODO hardcoded
-                print("Skipping", alignments[0][0], "due to multiple alignments", file=sys.stderr)
+                print(
+                    "Skipping",
+                    alignments[0][0],
+                    "due to multiple alignments",
+                    file=sys.stderr,
+                )
                 alignments = []
             else:
                 alignments.sort(key=lambda x: int(x[2]))
@@ -261,10 +302,13 @@ def main():
         smooth(alignments, reads[alignments[0][0]], out_fq, out_gaf)
     else:
         # TODO
-        print("Skipping", alignments[0][0], "due to multiple alignments", file=sys.stderr)
+        print(
+            "Skipping", alignments[0][0], "due to multiple alignments", file=sys.stderr
+        )
 
     out_fq.close()
     out_gaf.close()
+
 
 if __name__ == "__main__":
     # TODO argparse
