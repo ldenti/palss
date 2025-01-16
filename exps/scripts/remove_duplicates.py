44936e9a732e06dd10d50cec11486563a6f15b19
Luca Denti <ldenti@proton.me>
2025-01-16 14:58:36 +0100

    Print number of filtered duplicates

--- exps/scripts/remove_duplicates.py
+++ exps/scripts/remove_duplicates.py
@@ -16,17 +16,21 @@ def main():
 
     print(vcf.header, end="")
     kept = set()
+    tot=0
+    filt=0
     for record in vcf:
         v = get_var(record)
+        tot += 1
         if v in kept:
             v = ":".join(v.split(":")[:-1])
             print(f"Skipping {v}", file=sys.stderr)
+            filt += 1
             continue
         record.ref = record.ref.upper()
         record.alts = (x.upper() for x in record.alts)
         print(record, end="")
         kept.add(v)
-
+    print(filt, "/", tot, file=sys.stderr)
 
 if __name__ == "__main__":
     main()
