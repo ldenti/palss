938ed9deb446de3ad5c471c902349601ec418732
Luca Denti <ldenti@proton.me>
2026-01-29 15:09:04 +0100

    Fix mgcactus error when # is in FASTA headers and default to singularity

--- exps/utils/run_mgcactus.sh
+++ exps/utils/run_mgcactus.sh
@@ -18,9 +18,9 @@ echo -e "CHM13\t$FA" >> $WD/seqfile
 echo "# Diploid samples:" >> $WD/seqfile
 vg gbwt --gbz-input --samples --list-names $GBZ | grep -Pv "CHM13|GRCh38" | while read idx
 do
-    vg paths --paths-by "$idx#1" --extract-fasta --xg $GBZ > $WD/$idx.h1.fa
+    vg paths --paths-by "$idx#1" --extract-fasta --xg $GBZ | sed "s/#/_/g" > $WD/$idx.h1.fa
     echo -e "$idx.1\t$WD/$idx.h1.fa" >> $WD/seqfile
-    vg paths --paths-by "$idx#2" --extract-fasta --xg $GBZ > $WD/$idx.h2.fa
+    vg paths --paths-by "$idx#2" --extract-fasta --xg $GBZ | sed "s/#/_/g" > $WD/$idx.h2.fa
     echo -e "$idx.2\t$WD/$idx.h2.fa" >> $WD/seqfile
 done
 
@@ -28,7 +28,8 @@ echo -e "$name.1\t$HAP1" >> $WD/seqfile
 echo -e "$name.2\t$HAP2" >> $WD/seqfile
 
 rm -rf $WD/JOBSTORE
-/usr/bin/time -v cactus-pangenome $WD/JOBSTORE $WD/seqfile --outDir $WD --outName pangenome --reference CHM13 --maxCores $threads --logFile $WD/cactus-pangenome.log  # --binariesMode singularity
+mkdir -p $WD/tmptmptmp
+/usr/bin/time -vo $WD/../minigraph-cactus.time cactus-pangenome $WD/JOBSTORE $WD/seqfile --outDir $WD --workDir $WD/tmptmptmp --outName pangenome --reference CHM13 --maxCores $threads --logColors False --logFile $WD/cactus-pangenome.log --binariesMode singularity
 
 zcat $WD/pangenome.gfa.gz | head -1
 zgrep "^S" $WD/pangenome.gfa.gz | cut -f1-3
